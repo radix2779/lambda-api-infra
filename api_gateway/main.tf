1d4b9f16b6193efc0907aad453c9caaf0e2e56d3
radix2779 <vismay.chovatiya@radixweb.com>
2025-07-16 19:40:47 +0530

    Refactor API Gateway resources and methods for consistency and clarity

--- api_gateway/main.tf
+++ api_gateway/main.tf
@@ -7,28 +7,28 @@ variable "project_prefix" {}
 
 # https://www.terraform.io/docs/providers/aws/r/api_gateway_rest_api.html
 resource "aws_api_gateway_rest_api" "example_api" {
-  name = "example_api"
+  name = var.project_prefix
 }
 
 # equivalent to endpoint
 # AWS_BUG: if you set the path part ot 'test' it breaks!!
-resource "aws_api_gateway_resource" "example_resource" {
+resource "aws_api_gateway_resource" "api_resource" {
   rest_api_id = aws_api_gateway_rest_api.example_api.id
   parent_id   = aws_api_gateway_rest_api.example_api.root_resource_id
   path_part   = "helloworld"
 }
 
-resource "aws_api_gateway_method" "example_method" {
+resource "aws_api_gateway_method" "api_method" {
   rest_api_id   = aws_api_gateway_rest_api.example_api.id
-  resource_id   = aws_api_gateway_resource.example_resource.id
+  resource_id   = aws_api_gateway_resource.api_resource.id
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "example_integration" {
   rest_api_id = aws_api_gateway_rest_api.example_api.id
-  resource_id = aws_api_gateway_resource.example_resource.id
-  http_method = aws_api_gateway_method.example_method.http_method
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = aws_api_gateway_method.api_method.http_method
   # method that the api gateway will use to call the lambda - lambdas can only be invoked by POST, even though the gateway method may be a GET
   type                    = "AWS_PROXY"
   uri                     = var.lambda_json_invoke_arn
@@ -45,14 +45,14 @@ resource "aws_lambda_permission" "apigateway_lambda" {
   source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.example_api.id}/*/*"
 }
 
-resource "aws_api_gateway_deployment" "example_deployment" {
+resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on  = [aws_api_gateway_integration.example_integration]
   rest_api_id = aws_api_gateway_rest_api.example_api.id
 
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.example_resource.id,
-      aws_api_gateway_method.example_method.id,
+      aws_api_gateway_resource.api_resource.id,
+      aws_api_gateway_method.api_method.id,
       aws_api_gateway_integration.example_integration.id,
     ]))
   }
@@ -62,12 +62,12 @@ resource "aws_api_gateway_deployment" "example_deployment" {
   }
 }
 
-resource "aws_api_gateway_stage" "example_stage" {
-  deployment_id = aws_api_gateway_deployment.example_deployment.id
+resource "aws_api_gateway_stage" "api_stage" {
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.example_api.id
   stage_name    = var.project_prefix
 }
 
 output "lambda_public_url" {
-  value = "${aws_api_gateway_stage.example_stage.invoke_url}${aws_api_gateway_resource.example_resource.path_part}"
+  value = "${aws_api_gateway_stage.api_stage.invoke_url}${aws_api_gateway_resource.api_resource.path_part}"
 }
